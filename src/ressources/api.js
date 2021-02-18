5235f6e755b27495e36bb7502c85e1bd4d6c6a7f
soura934 <soura934@gmail.com>
2021-02-17 22:31:16 -0500

    api showing up now

--- src/ressources/api.js
+++ src/ressources/api.js
@@ -1,7 +1,7 @@
 
 
 console.log('hello');
-const nameOfCountry = document.querySelector(".country .name");
+const nameOfCountry = document.querySelector(".name");
 const casesOfCountry = document.querySelector(".cases .number");
 const newCasesOfCountry = document.querySelector(".cases .new-number");
 const deathOfCountry = document.querySelector(".death .number");
@@ -11,11 +11,12 @@ const newRecoverOfCountry = document.querySelector(".recover .new-number");
 
 // const ctx = document.getElementById("graph").getContext("2d");
 
-let dataApp = [],
-    listOfCases = [],
-    listOfRecover = [],
-    listOfDeath = [],
-    date = [];
+let dataApp,
+    cases ,
+    displayCountry,
+    recovered,
+    deaths,
+    date;
 
 
 let codeOfCountry = geoplugin_countryCode();
@@ -30,5 +31,33 @@ listOfCountries.forEach( country => {
 function fetchData(countryName){
     fetch(`https://disease.sh/v3/covid-19/countries/${countryName}`, {
 		"method": "GET"
-	})
+	}).then(response => {
+        return response.json();
+    }).then(data => {
+        // console.log(data.cases, data.country);
+        console.log(data);
+        // // all = Object.keys(data)
+        cases = data.cases;
+        deaths = data.deaths;
+        recovered = data.recovered
+        displayCountry = data.country;
+    }).then( () => {
+        updateHTML();
+    }).catch( error => {
+        console.log(error);
+    })
+}
+
+fetchData(countryName);
+
+function updateHTML(){
+    updateNumbers();
+    // updateChart();
+}
+
+function updateNumbers(){
+    nameOfCountry.innerHTML = displayCountry;
+    casesOfCountry.innerHTML = cases;
+    recoverOfCountry.innerHTML = recovered
+    deathOfCountry.innerHTML = deaths;
 }
